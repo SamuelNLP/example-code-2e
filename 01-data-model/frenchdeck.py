f0152abc8dfc2586a59834996fc362ab4cac8314
SamuelNLP <samuelnlpedro@gmail.com>
2020-02-17 12:48:33 +0000

    01-data-model: changed print to use f-strings

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -18,16 +18,11 @@ class FrenchDeck:
 
 
 if __name__ == "__main__":
-    suit_values = {
-        "spades": 3,
-        "hearts": 2,
-        "diamonds": 1,
-        "clubs": 0
-    }
-    
+    suit_values = {"spades": 3, "hearts": 2, "diamonds": 1, "clubs": 0}
+
     def spades_high(card: Card):
         rank_value = FrenchDeck.ranks.index(card.rank)
         return rank_value * len(suit_values) + suit_values[card.suit]
-    
+
     for card in sorted(FrenchDeck(), key=spades_high):
         print(card)
--- 01-data-model/vector2d.py
+++ 01-data-model/vector2d.py
@@ -32,14 +32,14 @@ Scalar multiplication::
 
 from math import hypot
 
-class Vector:
 
+class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
     def __repr__(self):
-        return 'Vector(%r, %r)' % (self.x, self.y)
+        return f"Vector({self.x}, {self.y})"
 
     def __abs__(self):
         return hypot(self.x, self.y)
